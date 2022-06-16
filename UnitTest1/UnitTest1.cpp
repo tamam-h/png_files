9a847e0d4428f864d7d689afa2997e8bd5274c90
tamam-h <74006672+tamam-h@users.noreply.github.com>
2022-06-16 15:43:10 -0500

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1 {
+	TEST_CLASS(UnitTest1) {
+	public:
+		TEST_METHOD(TestMethod1) {
+		}
+	};
+}
